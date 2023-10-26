f2597882e4c0889f3b974fc0599a275288ecec08
BpaYWH <yauwinghongbpa@gmail.com>
2023-10-26 17:10:54 -0400

    ADDED: signalr connection hook

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,7 @@
 import { createSignal } from "solid-js";
 import { Title } from "solid-start";
+
+import useJoin from "~/hooks/useJoin";
 import Board from "~/components/Board";
 
 const playerMap = {
@@ -11,6 +13,7 @@ const playerMap = {
 export default function Home() {
   const [boardState, setBoardState] = createSignal<number[][]>(new Array(15).fill(0).map(() => new Array(15).fill(0)));
   const [currPlayer, setCurrPlayer] = createSignal<number>(1); // 1: "●", 2: "○"
+  const { socket, msg } = useJoin();
 
   const updateBoard = (x: number, y: number) => { 
     const newBoardState = [...boardState()];
@@ -24,18 +27,30 @@ export default function Home() {
     setCurrPlayer(1);
   }
 
+  const sendMessage = () => {
+    socket()?.send("messageReceived", "Hi");
+    console.log("message sent");
+  }
+
   return (
     <main>
       <Title>Home</Title>
       <h1 class="cursor-pointer hover:drop-shadow transition" onClick={handleReset}>Gomoku</h1>
 
-      <div class="my-4">
+      <button 
+      class="px-4 py-2 border rounded-md hover:bg-slate-400 hover:text-white transition"
+      onClick={sendMessage}
+      >
+        Connect
+      </button>
+      <p>Connection: {msg()}</p>
+      {/* <div class="my-4">
         <h2>{playerMap[currPlayer() as 0 | 1 | 2]}</h2>
       </div>
       
       <div id="GameDiv" class="m-8">
           <Board updateBoard={updateBoard} boardState={boardState()} playerMap={playerMap} />
-      </div>
+      </div> */}
     </main>
   );
 }
